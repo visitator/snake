b7082b989b0af0d8f4549edc3c34449e6eab5ba2
visitator <rooll679@yandex.ru>
2016-12-08 19:55:43 +0300

    Revert "Revert "we""
    This reverts commit 5684a70511dcd41b94010c64733fc72a4faaf627.

--- snake/js/config.js
+++
@@ -1,4 +0,0 @@
-/*var matrixes = parseInt($(':radio[name=matrixes]:checked').val());
-var rows = parseInt($(':text[id=rows]').val());
-var cols = parseInt($(':text[id=cols]').val());
-//var speed = $('')*/
