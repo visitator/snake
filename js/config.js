729b8ccf6ac010acd5ecf776d9c417eb72f722a1
visitator <rooll679@yandex.ru>
2016-12-08 19:53:19 +0300

    we
    we

---
+++ js/config.js
@@ -0,0 +1,4 @@
+/*var matrixes = parseInt($(':radio[name=matrixes]:checked').val());
+var rows = parseInt($(':text[id=rows]').val());
+var cols = parseInt($(':text[id=cols]').val());
+//var speed = $('')*/
